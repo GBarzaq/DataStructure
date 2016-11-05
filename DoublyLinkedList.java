e8b97cfd637f46640fec10f0a4cd702deb06d70b
GBarzaq <0ago0barzaq@gmail.com>
2016-11-05 21:05:59 +0200

    Update DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,7 +1,21 @@
-public Class DoublyLinkedList <E>{
-  private static Class Node<E>{
+public class DoublyLinkedList <E>{
+  private static class Node<E>{
     E e;
-    Node next;
-    Node prev;
+    Node<E> next;
+    Node<E> prev;
+    public Node(E e, Node<E> prev, Node<E> next){
+      this.e = e;
+      this.prev = prev;
+      this.next = next;
+    }
+    public E getElement() { return element; }
+
+    public Node<E> getPrev() { return prev; }
+    
+    public Node<E> getNext() { return next; }
+    
+    public void setPrev(Node<E> p) { prev = p; }
+
+    public void setNext(Node<E> n) { next = n; }
   }
 }
